090836cad44409b55c6fbf2270416617d663586f
Mathias Buus <mathiasbuus@gmail.com>
2016-11-25 20:17:46 -0300

    basic coverage for crypto_sign

--- test/crypto_sign.js
+++ test/crypto_sign.js
@@ -46,6 +46,45 @@ tape('crypto_sign_keypair', function (t) {
   t.end()
 })
 
+tape('crypto_sign', function (t) {
+  var pk = new Buffer(sodium.crypto_sign_PUBLICKEYBYTES)
+  var sk = new Buffer(sodium.crypto_sign_SECRETKEYBYTES)
+
+  sodium.crypto_sign_keypair(pk, sk)
+
+  var message = new Buffer('hello world')
+  var signedMessage = new Buffer(message.length + sodium.crypto_sign_BYTES)
+
+  sodium.crypto_sign(signedMessage, message, sk)
+
+  t.same(signedMessage.slice(-message.length), message, 'contains message')
+
+  var output = new Buffer(message.length)
+
+  t.notOk(sodium.crypto_sign_open(output, Buffer.concat([blank(1), signedMessage]), pk), 'was not signed')
+  t.ok(sodium.crypto_sign_open(output, signedMessage, pk), 'was signed')
+
+  t.same(output, message, 'same message')
+  t.end()
+})
+
+tape('crypto_sign_detached', function (t) {
+  var pk = new Buffer(sodium.crypto_sign_PUBLICKEYBYTES)
+  var sk = new Buffer(sodium.crypto_sign_SECRETKEYBYTES)
+
+  sodium.crypto_sign_keypair(pk, sk)
+
+  var message = new Buffer('hello world')
+  var signature = new Buffer(sodium.crypto_sign_BYTES)
+
+  sodium.crypto_sign_detached(signature, message, sk)
+
+  t.notOk(sodium.crypto_sign_verify_detached(Buffer.concat([blank(1), signature]), message, pk), 'was not signed')
+  t.ok(sodium.crypto_sign_verify_detached(signature, message, pk), 'was signed')
+
+  t.end()
+})
+
 function blank (n) {
   var buf = new Buffer(n)
   buf.fill(0)
