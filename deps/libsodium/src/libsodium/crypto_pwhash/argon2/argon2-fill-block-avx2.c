5571de796f62552298d380064ef307392c35eb6a
Emil Bay <github@tixz.dk>
2017-08-06 18:52:09 +0200

    Upgrade to libsodium 1.0.13

--- deps/libsodium/src/libsodium/crypto_pwhash/argon2/argon2-fill-block-avx2.c
+++ deps/libsodium/src/libsodium/crypto_pwhash/argon2/argon2-fill-block-avx2.c
@@ -20,79 +20,80 @@
 #include "private/common.h"
 #include "private/sse2_64_32.h"
 
-#if defined(HAVE_EMMINTRIN_H) && defined(HAVE_TMMINTRIN_H)
+#if defined(HAVE_AVX2INTRIN_H) && defined(HAVE_EMMINTRIN_H) && \
+    defined(HAVE_TMMINTRIN_H) && defined(HAVE_SMMINTRIN_H)
 
 # ifdef __GNUC__
 #  pragma GCC target("sse2")
 #  pragma GCC target("ssse3")
+#  pragma GCC target("sse4.1")
+#  pragma GCC target("avx2")
 # endif
 
 # ifdef _MSC_VER
 #  include <intrin.h> /* for _mm_set_epi64x */
 # endif
-# include <emmintrin.h>
-# include <tmmintrin.h>
+#include <emmintrin.h>
+#include <immintrin.h>
+#include <smmintrin.h>
+#include <tmmintrin.h>
 
-# include "blamka-round-ssse3.h"
+# include "blamka-round-avx2.h"
 
 static void
-fill_block(__m128i *state, const uint8_t *ref_block, uint8_t *next_block)
+fill_block(__m256i *state, const uint8_t *ref_block, uint8_t *next_block)
 {
-    __m128i  block_XY[ARGON2_OWORDS_IN_BLOCK];
+    __m256i  block_XY[ARGON2_HWORDS_IN_BLOCK];
     uint32_t i;
 
-    for (i = 0; i < ARGON2_OWORDS_IN_BLOCK; i++) {
-        block_XY[i] = state[i] = _mm_xor_si128(
-            state[i], _mm_loadu_si128((__m128i const *) (&ref_block[16 * i])));
+    for (i = 0; i < ARGON2_HWORDS_IN_BLOCK; i++) {
+        block_XY[i] = state[i] = _mm256_xor_si256(
+            state[i], _mm256_loadu_si256((__m256i const *) (&ref_block[32 * i])));
     }
 
-    for (i = 0; i < 8; ++i) {
-        BLAKE2_ROUND(state[8 * i + 0], state[8 * i + 1], state[8 * i + 2],
-                     state[8 * i + 3], state[8 * i + 4], state[8 * i + 5],
-                     state[8 * i + 6], state[8 * i + 7]);
+    for (i = 0; i < 4; ++i) {
+        BLAKE2_ROUND_1(state[8 * i + 0], state[8 * i + 4], state[8 * i + 1], state[8 * i + 5],
+                       state[8 * i + 2], state[8 * i + 6], state[8 * i + 3], state[8 * i + 7]);
     }
 
-    for (i = 0; i < 8; ++i) {
-        BLAKE2_ROUND(state[8 * 0 + i], state[8 * 1 + i], state[8 * 2 + i],
-                     state[8 * 3 + i], state[8 * 4 + i], state[8 * 5 + i],
-                     state[8 * 6 + i], state[8 * 7 + i]);
+    for (i = 0; i < 4; ++i) {
+        BLAKE2_ROUND_2(state[ 0 + i], state[ 4 + i], state[ 8 + i], state[12 + i],
+                       state[16 + i], state[20 + i], state[24 + i], state[28 + i]);
     }
 
-    for (i = 0; i < ARGON2_OWORDS_IN_BLOCK; i++) {
-        state[i] = _mm_xor_si128(state[i], block_XY[i]);
-        _mm_storeu_si128((__m128i *) (&next_block[16 * i]), state[i]);
+    for (i = 0; i < ARGON2_HWORDS_IN_BLOCK; i++) {
+        state[i] = _mm256_xor_si256(state[i], block_XY[i]);
+        _mm256_storeu_si256((__m256i *) (&next_block[32 * i]), state[i]);
     }
 }
 
 static void
-fill_block_with_xor(__m128i *state, const uint8_t *ref_block,
+fill_block_with_xor(__m256i *state, const uint8_t *ref_block,
                     uint8_t *next_block)
 {
-    __m128i  block_XY[ARGON2_OWORDS_IN_BLOCK];
+    __m256i  block_XY[ARGON2_HWORDS_IN_BLOCK];
     uint32_t i;
 
-    for (i = 0; i < ARGON2_OWORDS_IN_BLOCK; i++) {
-        state[i] = _mm_xor_si128(
-            state[i], _mm_loadu_si128((__m128i const *) (&ref_block[16 * i])));
-        block_XY[i] = _mm_xor_si128(
-            state[i], _mm_loadu_si128((__m128i const *) (&next_block[16 * i])));
+    for (i = 0; i < ARGON2_HWORDS_IN_BLOCK; i++) {
+        state[i] = _mm256_xor_si256(
+            state[i], _mm256_loadu_si256((__m256i const *) (&ref_block[32 * i])));
+        block_XY[i] = _mm256_xor_si256(
+            state[i], _mm256_loadu_si256((__m256i const *) (&next_block[32 * i])));
     }
 
-    for (i = 0; i < 8; ++i) {
-        BLAKE2_ROUND(state[8 * i + 0], state[8 * i + 1], state[8 * i + 2],
-                     state[8 * i + 3], state[8 * i + 4], state[8 * i + 5],
-                     state[8 * i + 6], state[8 * i + 7]);
+    for (i = 0; i < 4; ++i) {
+        BLAKE2_ROUND_1(state[8 * i + 0], state[8 * i + 4], state[8 * i + 1], state[8 * i + 5],
+                       state[8 * i + 2], state[8 * i + 6], state[8 * i + 3], state[8 * i + 7]);
     }
 
-    for (i = 0; i < 8; ++i) {
-        BLAKE2_ROUND(state[8 * 0 + i], state[8 * 1 + i], state[8 * 2 + i],
-                     state[8 * 3 + i], state[8 * 4 + i], state[8 * 5 + i],
-                     state[8 * 6 + i], state[8 * 7 + i]);
+    for (i = 0; i < 4; ++i) {
+        BLAKE2_ROUND_2(state[ 0 + i], state[ 4 + i], state[ 8 + i], state[12 + i],
+                       state[16 + i], state[20 + i], state[24 + i], state[28 + i]);
     }
 
-    for (i = 0; i < ARGON2_OWORDS_IN_BLOCK; i++) {
-        state[i] = _mm_xor_si128(state[i], block_XY[i]);
-        _mm_storeu_si128((__m128i *) (&next_block[16 * i]), state[i]);
+    for (i = 0; i < ARGON2_HWORDS_IN_BLOCK; i++) {
+        state[i] = _mm256_xor_si256(state[i], block_XY[i]);
+        _mm256_storeu_si256((__m256i *) (&next_block[32 * i]), state[i]);
     }
 }
 
@@ -117,8 +118,8 @@ generate_addresses(const argon2_instance_t *instance,
         for (i = 0; i < instance->segment_length; ++i) {
             if (i % ARGON2_ADDRESSES_IN_BLOCK == 0) {
                 /* Temporary zero-initialized blocks */
-                __m128i zero_block[ARGON2_OWORDS_IN_BLOCK];
-                __m128i zero2_block[ARGON2_OWORDS_IN_BLOCK];
+                __m256i zero_block[ARGON2_HWORDS_IN_BLOCK];
+                __m256i zero2_block[ARGON2_HWORDS_IN_BLOCK];
 
                 memset(zero_block, 0, sizeof(zero_block));
                 memset(zero2_block, 0, sizeof(zero2_block));
@@ -140,14 +141,14 @@ generate_addresses(const argon2_instance_t *instance,
 }
 
 int
-fill_segment_ssse3(const argon2_instance_t *instance,
-                   argon2_position_t        position)
+fill_segment_avx2(const argon2_instance_t *instance,
+                  argon2_position_t        position)
 {
     block    *ref_block = NULL, *curr_block = NULL;
     uint64_t  pseudo_rand, ref_index, ref_lane;
     uint32_t  prev_offset, curr_offset;
     uint32_t  starting_index, i;
-    __m128i   state[64];
+    __m256i   state[32];
     int       data_independent_addressing = 1;
 
     /* Pseudo-random values that determine the reference block position */
