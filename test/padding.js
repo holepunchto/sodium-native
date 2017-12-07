9f2fa78edbc2ff0a4141a53d8e8390ae00647d16
Emil Bay <github@tixz.dk>
2017-12-07 13:54:18 +0100

    Make test less noisy

--- test/padding.js
+++ test/padding.js
@@ -11,23 +11,45 @@ test('sodium_pad / sodium_unpad', function (assert) {
     var binPaddedLen = bingPaddedLong.slice(0, binPaddedMaxlen)
     sodium.randombytes_buf(binPaddedLen)
 
-    assert.throws(function () {
+    var smallThrow = didThrow(function () {
       sodium.sodium_pad(binPaddedLen.slice(0, binPaddedMaxlen - 1), binLen, blocksize)
+    })
+    if (smallThrow === false) assert.fail('did not throw')
+
+    var zeroThrow = didThrow(function () {
       sodium.sodium_pad(binPaddedLen, binLen, 0)
     })
+    if (zeroThrow === false) assert.fail('did not throw')
 
     sodium.sodium_pad(bingPaddedLong, binLen, blocksize)
     var binUnpaddedLen = sodium.sodium_pad(binPaddedLen, binLen, blocksize)
-    assert.equal(binUnpaddedLen, binPaddedMaxlen)
+    if (binUnpaddedLen !== binPaddedMaxlen) assert.fail('binUnpaddedLen was not same')
 
-    assert.throws(function () {
+
+    var largeThrow = didThrow(function () {
       sodium.sodium_unpad(binPaddedLen, binUnpaddedLen, binPaddedMaxlen + 1)
+    })
+    if (largeThrow === false) assert.fail('did not throw')
+
+    var emptyThrow = didThrow(function () {
       sodium.sodium_unpad(binPaddedLen, binUnpaddedLen, 0)
     })
+    if (emptyThrow === false) assert.fail('did not throw')
+
 
     var len2 = sodium.sodium_unpad(binPaddedLen, binUnpaddedLen, blocksize)
-    assert.equal(len2, binLen)
+    if(len2 !== binLen) assert.fail('len2 was not same')
   }
 
+  assert.pass()
   assert.end()
 })
+
+function didThrow (fn) {
+  try {
+    fn()
+    return false
+  } catch (ex) {
+    return true
+  }
+}
