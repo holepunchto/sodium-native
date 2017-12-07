d117a0571b8055b576433ab54d3436437db0c09a
Emil Bay <github@tixz.dk>
2017-12-07 07:31:59 +0100

    Be standard

--- test/padding.js
+++ test/padding.js
@@ -4,30 +4,29 @@ var sodium = require('../')
 
 test('sodium_pad / sodium_unpad', function (assert) {
   for (var i = 0; i < 2000; i++) {
-    var bin_len = Math.random() * 200 | 0 // FIXME
+    var binLen = Math.random() * 200 | 0 // FIXME
     var blocksize = 1 + Math.random() * 100 | 0
-    var bin_padded_maxlen = bin_len + (blocksize - (bin_len % blocksize))
-    var bin_padded_long = alloc(bin_padded_maxlen + 1)
-    var bin_padded = bin_padded_long.slice(0, bin_padded_maxlen)
-    sodium.randombytes_buf(bin_padded)
+    var binPaddedMaxlen = binLen + (blocksize - (binLen % blocksize))
+    var bingPaddedLong = alloc(binPaddedMaxlen + 1)
+    var binPaddedLen = bingPaddedLong.slice(0, binPaddedMaxlen)
+    sodium.randombytes_buf(binPaddedLen)
 
     assert.throws(function () {
-      sodium.sodium_pad(bin_padded.slice(0, bin_padded_maxlen - 1), bin_len, blocksize)
-      sodium.sodium_pad(bin_padded, bin_len, 0)
+      sodium.sodium_pad(binPaddedLen.slice(0, binPaddedMaxlen - 1), binLen, blocksize)
+      sodium.sodium_pad(binPaddedLen, binLen, 0)
     })
 
-    sodium.sodium_pad(bin_padded_long, bin_len, blocksize)
-    var bin_padded_len = sodium.sodium_pad(bin_padded, bin_len, blocksize)
-    assert.equal(bin_padded_len, bin_padded_maxlen)
+    sodium.sodium_pad(bingPaddedLong, binLen, blocksize)
+    var binUnpaddedLen = sodium.sodium_pad(binPaddedLen, binLen, blocksize)
+    assert.equal(binUnpaddedLen, binPaddedMaxlen)
 
     assert.throws(function () {
-      sodium.sodium_unpad(bin_padded, bin_padded_len, bin_padded_maxlen + 1)
-      sodium.sodium_unpad(bin_padded, bin_padded_len, 0)
+      sodium.sodium_unpad(binPaddedLen, binUnpaddedLen, binPaddedMaxlen + 1)
+      sodium.sodium_unpad(binPaddedLen, binUnpaddedLen, 0)
     })
 
-    var len2 = sodium.sodium_unpad(bin_padded, bin_padded_len, blocksize)
-    assert.equal(len2, bin_len)
-
+    var len2 = sodium.sodium_unpad(binPaddedLen, binUnpaddedLen, blocksize)
+    assert.equal(len2, binLen)
   }
 
   assert.end()
