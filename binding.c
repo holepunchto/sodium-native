c3250c1b01f8db40ef1fd6f59636d3aa5c3116ff
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-02-10 13:12:18 +0100

    pad, randombytes, randombytes_buf_deterministic added

--- binding.c
+++ binding.c
@@ -4,16 +4,15 @@
 #include <sodium.h>
 
 napi_value sn_randombytes_random (napi_env env, napi_callback_info info) {
-  uint32_t upp
-
   napi_value result;
+
   assert(napi_create_uint32(env, randombytes_random(), &result) == napi_ok);
   return result;
 }
 
 napi_value sn_randombytes_uniform (napi_env env, napi_callback_info info) {
-  NAPI_ARGV(1, randombytes_uniform);
-  NAPI_TYPE_ASSERT(upper_bound, argv[0], napi_number, "upper_bound must be a Number");
+  SN_ARGV(1, randombytes_uniform);
+  SN_TYPE_ASSERT(upper_bound, argv[0], napi_number, "upper_bound must be a Number");
 
   uint32_t upper_bound;
   assert(napi_get_value_uint32(env, argv[0], &upper_bound) == napi_ok);
@@ -40,15 +39,37 @@ uint8_t typedarray_width(napi_typedarray_type type) {
   }
 }
 
+napi_value sn_randombytes_buf (napi_env env, napi_callback_info info) {
+  SN_ARGV(1, randombytes_buf)
+
+  SN_ARGV_TYPEDARRAY(buf, 0, "buf must be an TypedArray")
+
+  randombytes_buf(buf_data, buf_size);
+
+  return NULL;
+}
+
+
+napi_value sn_randombytes_buf_deterministic (napi_env env, napi_callback_info info) {
+  SN_ARGV(2, randombytes_buf)
+
+  SN_ARGV_TYPEDARRAY(buf, 0, "buf must be an TypedArray")
+  SN_ARGV_TYPEDARRAY(seed, 1, "seed must be a TypedArray")
+  
+  SN_THROWS(seed_size != randombytes_SEEDBYTES, "seed must be 32 bytes")
+
+  randombytes_buf_deterministic(buf_data, buf_size, seed_data);
+
+  return NULL;
+}
+
 napi_value sn_sodium_memcmp(napi_env env, napi_callback_info info) {
-  NAPI_ARGV(2, sodium_memcmp);
-  NAPI_TYPEDARRAY_ASSERT(b1, argv[0], "b1 must be instance of TypedArray");
-  NAPI_TYPEDARRAY_ASSERT(b2, argv[1], "b1 must be instance of TypedArray");
+  SN_ARGV(2, sodium_memcmp);
 
-  NAPI_TYPEDARRAY(b1, argv[0])
-  NAPI_TYPEDARRAY(b2, argv[1])
+  SN_ARGV_TYPEDARRAY(b1, 0, "b1 must be instance of TypedArray");
+  SN_ARGV_TYPEDARRAY(b2, 1, "b2 must be instance of TypedArray");
 
-  NAPI_THROWS(b1_size != b2_size, "buffers must be of same length");
+  SN_THROWS(b1_size != b2_size, "buffers must be of same length");
 
   int cmp = sodium_memcmp(b1_data, b2_data, b1_size);
 
@@ -58,10 +79,8 @@ napi_value sn_sodium_memcmp(napi_env env, napi_callback_info info) {
 }
 
 napi_value sn_sodium_increment(napi_env env, napi_callback_info info) {
-  NAPI_ARGV(1, sodium_increment);
-  NAPI_TYPEDARRAY_ASSERT(n, argv[0], "n must be an instance of TypedArray");
-
-  NAPI_TYPEDARRAY(n, argv[0]);
+  SN_ARGV(1, sodium_increment);
+  SN_ARGV_TYPEDARRAY(n, 0, "n must be an instance of TypedArray");
 
   sodium_increment(n_data, n_size);
 
@@ -69,42 +88,36 @@ napi_value sn_sodium_increment(napi_env env, napi_callback_info info) {
 }
 
 napi_value sn_sodium_add(napi_env env, napi_callback_info info) {
-  NAPI_ARGV(2, sodium_add);
-  NAPI_TYPEDARRAY_ASSERT(a, argv[0], "a must be an instance of TypedArray");
-  NAPI_TYPEDARRAY_ASSERT(b, argv[1], "b must be an instance of TypedArray");
+  SN_ARGV(2, sodium_add);
 
-  NAPI_TYPEDARRAY(a, argv[0]);
-  NAPI_TYPEDARRAY(b, argv[1]);
+  SN_ARGV_TYPEDARRAY(a, 0, "a must be an instance of TypedArray");
+  SN_ARGV_TYPEDARRAY(b, 1, "b must be an instance of TypedArray");
 
-  NAPI_THROWS(a_size != b_size, "buffers must be of same length");
+  SN_THROWS(a_size != b_size, "buffers must be of same length");
   sodium_add(a_data, b_data, a_size);
 
   return NULL;
 }
 
 napi_value sn_sodium_sub(napi_env env, napi_callback_info info) {
-  NAPI_ARGV(2, sodium_sub);
-  NAPI_TYPEDARRAY_ASSERT(a, argv[0], "a must be an instance of TypedArray");
-  NAPI_TYPEDARRAY_ASSERT(b, argv[1], "b must be an instance of TypedArray");
+  SN_ARGV(2, sodium_sub);
 
-  NAPI_TYPEDARRAY(a, argv[0]);
-  NAPI_TYPEDARRAY(b, argv[1]);
+  SN_ARGV_TYPEDARRAY(a, 0, "a must be an instance of TypedArray");
+  SN_ARGV_TYPEDARRAY(b, 1, "b must be an instance of TypedArray");
 
-  NAPI_THROWS(a_size != b_size, "buffers must be of same length");
+  SN_THROWS(a_size != b_size, "buffers must be of same length");
   sodium_sub(a_data, b_data, a_size);
 
   return NULL;
 }
 
 napi_value sn_sodium_compare(napi_env env, napi_callback_info info) {
-  NAPI_ARGV(2, sodium_compare);
-  NAPI_TYPEDARRAY_ASSERT(a, argv[0], "a must be an instance of TypedArray");
-  NAPI_TYPEDARRAY_ASSERT(b, argv[1], "b must be an instance of TypedArray");
+  SN_ARGV(2, sodium_compare);
 
-  NAPI_TYPEDARRAY(a, argv[0]);
-  NAPI_TYPEDARRAY(b, argv[1]);
+  SN_ARGV_TYPEDARRAY(a, 0, "a must be an instance of TypedArray");
+  SN_ARGV_TYPEDARRAY(b, 1, "b must be an instance of TypedArray");
 
-  NAPI_THROWS(a_size != b_size, "buffers must be of same length");
+  SN_THROWS(a_size != b_size, "buffers must be of same length");
   int cmp = sodium_compare(a_data, b_data, a_size);
 
   napi_value result;
@@ -114,10 +127,9 @@ napi_value sn_sodium_compare(napi_env env, napi_callback_info info) {
 }
 
 napi_value sn_sodium_is_zero(napi_env env, napi_callback_info info) {
-  NAPI_ARGV(1, sodium_is_zero);
-  NAPI_TYPEDARRAY_ASSERT(a, argv[0], "a must be an instance of TypedArray");
+  SN_ARGV(1, sodium_is_zero);
 
-  NAPI_TYPEDARRAY(a, argv[0]);
+  SN_ARGV_TYPEDARRAY(a, 0, "a must be an instance of TypedArray");
 
   sodium_is_zero(a_data, a_size);
   int cmp = sodium_is_zero(a_data, a_size);
@@ -127,18 +139,34 @@ napi_value sn_sodium_is_zero(napi_env env, napi_callback_info info) {
   return result;
 }
 
+napi_value sn_sodium_pad(napi_env env, napi_callback_info info) {
+  SN_ARGV(3, sodium_pad);
+
+  SN_ARGV_TYPEDARRAY(buf, 0, "buf must be an instance of TypedArray");
+  SN_TYPE_ASSERT(unpadded_buflen, argv[1], napi_number, "unpadded_buflen must be a Number");
+
+  uint32_t unpadded_buflen;
+  assert(napi_get_value_uint32(env, argv[1], &unpadded_buflen) == napi_ok);
+
+  printf("%u\n", unpadded_buflen);
+  // SN_UINT32_ASSERT(unpadded_buflen, argv[1]);
+}
+
 napi_value create_sodium_native(napi_env env) {
   napi_value exports;
   assert(napi_create_object(env, &exports) == napi_ok);
 
-  NAPI_EXPORT_FUNCTION(randombytes_uniform, sn_randombytes_uniform)
-  NAPI_EXPORT_FUNCTION(randombytes_random, sn_randombytes_random)
-  NAPI_EXPORT_FUNCTION(sodium_memcmp, sn_sodium_memcmp)
-  NAPI_EXPORT_FUNCTION(sodium_increment, sn_sodium_increment)
-  NAPI_EXPORT_FUNCTION(sodium_add, sn_sodium_add)
-  NAPI_EXPORT_FUNCTION(sodium_sub, sn_sodium_sub)
-  NAPI_EXPORT_FUNCTION(sodium_compare, sn_sodium_compare)
-  NAPI_EXPORT_FUNCTION(sodium_is_zero, sn_sodium_is_zero)
+  SN_EXPORT_FUNCTION(randombytes_buf, sn_randombytes_buf)
+  SN_EXPORT_FUNCTION(randombytes_buf_deterministic, sn_randombytes_buf_deterministic)
+  SN_EXPORT_FUNCTION(randombytes_uniform, sn_randombytes_uniform)
+  SN_EXPORT_FUNCTION(randombytes_random, sn_randombytes_random)
+  SN_EXPORT_FUNCTION(sodium_memcmp, sn_sodium_memcmp)
+  SN_EXPORT_FUNCTION(sodium_increment, sn_sodium_increment)
+  SN_EXPORT_FUNCTION(sodium_add, sn_sodium_add)
+  SN_EXPORT_FUNCTION(sodium_sub, sn_sodium_sub)
+  SN_EXPORT_FUNCTION(sodium_compare, sn_sodium_compare)
+  SN_EXPORT_FUNCTION(sodium_is_zero, sn_sodium_is_zero)
+  SN_EXPORT_FUNCTION(sodium_pad, sn_sodium_pad)
 
   return exports;
 }
--- macros.h
+++ macros.h
@@ -1,65 +1,80 @@
-#define NAPI_STATUS_THROWS(call, message) \
+#define SN_STATUS_THROWS(call, message) \
   if ((call) != napi_ok) { \
     napi_throw_error(env, NULL, message); \
     return NULL; \
   }
 
-#define NAPI_THROWS(condition, message) \
+#define SN_THROWS(condition, message) \
   if ((condition)) { \
     napi_throw_error(env, NULL, message); \
     return NULL; \
   }
 
-#define NAPI_TYPE_ASSERT(name, var, type, message) \
+#define SN_UINT32_ASSERT(name, val) \
+  uint32_t name; \
+  if (napi_get_value_uint32(env, val, &name) != napi_ok) { \
+    napi_throw_error(env, "EINVAL", "Expected unsigned number"); \
+    return NULL; \
+  }; \
+  printf("%u, %f", napi_get_value_uint32(env, val, &name), val); \
+
+
+#define SN_TYPE_ASSERT(name, var, type, message) \
   napi_valuetype name##_valuetype; \
-  NAPI_STATUS_THROWS(napi_typeof(env, var, &name##_valuetype), ""); \
+  SN_STATUS_THROWS(napi_typeof(env, var, &name##_valuetype), ""); \
   if (name##_valuetype != type) { \
     napi_throw_type_error(env, NULL, message); \
     return NULL; \
   }
 
-#define NAPI_TYPEDARRAY_ASSERT(name, var, message) \
+#define SN_TYPEDARRAY_ASSERT(name, var, message) \
   bool name##_is_typedarray; \
-  NAPI_STATUS_THROWS(napi_is_typedarray(env, var, &name##_is_typedarray), ""); \
+  SN_STATUS_THROWS(napi_is_typedarray(env, var, &name##_is_typedarray), ""); \
   if (name##_is_typedarray != true) { \
     napi_throw_type_error(env, NULL, message); \
     return NULL; \
   }
 
-#define NAPI_RANGE_THROWS(condition, message) \
+#define SN_RANGE_THROWS(condition, message) \
   if (condition) { \
     napi_throw_range_error(env, NULL, message); \
     return NULL; \
   }
 
-#define NAPI_ARGV(n, method_name) \
+#define SN_ARGV(n, method_name) \
   napi_value argv[n]; \
   size_t argc = n; \
-  NAPI_STATUS_THROWS(napi_get_cb_info(env, info, &argc, argv, NULL, NULL), ""); \
+  SN_STATUS_THROWS(napi_get_cb_info(env, info, &argc, argv, NULL, NULL), ""); \
   if (argc < n) { \
     napi_throw_type_error(env, NULL, #method_name " requires at least " #n " argument(s)"); \
     return NULL; \
   }
 
-#define NAPI_EXPORT_FUNCTION(name, cb) \
+#define SN_EXPORT_FUNCTION(name, cb) \
   { \
     napi_value name##_fn; \
-    NAPI_STATUS_THROWS(napi_create_function(env, #name, NAPI_AUTO_LENGTH, cb, NULL, &name##_fn), "") \
-    NAPI_STATUS_THROWS(napi_set_named_property(env, exports, #name, name##_fn), "") \
+    SN_STATUS_THROWS(napi_create_function(env, #name, NAPI_AUTO_LENGTH, cb, NULL, &name##_fn), "") \
+    SN_STATUS_THROWS(napi_set_named_property(env, exports, #name, name##_fn), "") \
   }
 
-#define NAPI_EXPORT_UINT32(name, num) \
+#define SN_EXPORT_UINT32(name, num) \
   { \
     napi_value name##_num; \
-    NAPI_STATUS_THROWS(napi_create_uint32(env, num, &name##_num), "") \
-    NAPI_STATUS_THROWS(napi_set_named_property(env, exports, #name, name##_num), "") \
+    SN_STATUS_THROWS(napi_create_uint32(env, num, &name##_num), "") \
+    SN_STATUS_THROWS(napi_set_named_property(env, exports, #name, name##_num), "") \
   }
 
-#define NAPI_TYPEDARRAY(name, var) \
+#define SN_TYPEDARRAY(name, var) \
   napi_typedarray_type name##_type; \
   size_t name##_length; \
   void * name##_data; \
   assert(napi_get_typedarray_info(env, (var), &name##_type, &name##_length, &name##_data, NULL, NULL) == napi_ok); \
   uint8_t name##_width = typedarray_width(name##_type); \
-  NAPI_THROWS(name##_width == 0, "Unexpected TypedArray type"); \
+  SN_THROWS(name##_width == 0, "Unexpected TypedArray type") \
   size_t name##_size = name##_length * name##_width;
+
+#define SN_ARGV_TYPEDARRAY(name, index, message) \
+  napi_value name##_argv = argv[index]; \
+  SN_TYPEDARRAY_ASSERT(name, name##_argv, message) \
+  SN_TYPEDARRAY(name, name##_argv)
+
