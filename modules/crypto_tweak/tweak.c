af41abc99fab9b022a17b2bd4598f3f64396073a
Christophe Diederichs <chm-diederichs@protonmail.com>
2022-09-08 11:30:37 +0100

    use SN_TWEAK namespace

--- modules/crypto_tweak/tweak.c
+++ modules/crypto_tweak/tweak.c
@@ -68,7 +68,7 @@ void crypto_tweak_ed25519(unsigned char *n, unsigned char *q,
     crypto_scalarmult_ed25519_base_noclamp(q, n64);
   }
 
-  SN_COPY_32(n, n64)
+  SN_TWEAK_COPY_32(n, n64)
 }
 
 int crypto_tweak_ed25519_sign_detached(unsigned char *sig, unsigned long long *siglen_p,
@@ -110,7 +110,7 @@ int crypto_tweak_ed25519_sign_detached(unsigned char *sig, unsigned long long *s
   crypto_core_ed25519_scalar_mul(sig, hram, sk);
   crypto_core_ed25519_scalar_add(sig + 32, nonce, sig);
 
-  SN_COPY_32(sig, R)
+  SN_TWEAK_COPY_32(sig, R)
 
   if (siglen_p != NULL) {
     *siglen_p = 64U;
@@ -130,7 +130,7 @@ void crypto_tweak_ed25519_sk_to_scalar(unsigned char *n, const unsigned char *sk
   n64[31] &= 127;
   n64[31] |= 64;
 
-  SN_COPY_32(n, n64)
+  SN_TWEAK_COPY_32(n, n64)
 }
 
 // tweak a secret key
