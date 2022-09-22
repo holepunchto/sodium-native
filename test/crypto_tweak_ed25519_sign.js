9ee0f6cb83442b374fbe62d6d8d243be211a7ef7
Christophe Diederichs <chm-diederichs@protonmail.com>
2022-09-22 17:57:07 +0100

    add fixture tests for crypto_tweak_keypair

--- test/crypto_tweak_ed25519_sign.js
+++ test/crypto_tweak_ed25519_sign.js
@@ -4,16 +4,21 @@ const fixtures = require('./fixtures/crypto_tweak_ed25519_sign.js')
 
 tape('crypto_tweak sign fixtures', t => {
   for (const f of fixtures) {
-    const [sk, n, m, sig] = f.map(Buffer.from)
+    const [sk, n, m, sig, tweak, tpk, tn] = f.map(Buffer.from)
 
     const signature = Buffer.alloc(64)
     const scalar = Buffer.alloc(32)
+    const pk = Buffer.alloc(32)
 
     sodium.experimental_crypto_tweak_ed25519_sk_to_scalar(scalar, sk)
     t.same(scalar, n)
 
     sodium.experimental_crypto_tweak_ed25519_sign_detached(signature, m, n)
     t.same(signature, sig)
+
+    sodium.experimental_crypto_tweak_ed25519_keypair(pk, scalar, n, tweak)
+    t.same(pk, tpk)
+    t.same(scalar, tn)
   }
   t.end()
 })
