15837bc84fe5727dfef39b26e8a8099a1f121239
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2022-09-27 15:14:27 +0100

    Add `crypto_tweak_keypair` method and differentiate between `secretKey` and `scalar` (#164)
    * add crypto_tweak method for keypairs
    
    * rename secretkey_add to scalar_add
    
    * replace references to sk instead of scalar
    
    * tweak_secretkey calls to sk_to_scalar
    
    * tweak_ed25519 calls to _tweak_ed25519
    
    * add tweak_keypair test

--- modules/crypto_tweak/tweak.c
+++ modules/crypto_tweak/tweak.c
@@ -10,7 +10,7 @@
   Use at your own risk
 */
 
-void _crypto_tweak_nonce (unsigned char *nonce, const unsigned char *sk,
+void _crypto_tweak_nonce (unsigned char *nonce, const unsigned char *n,
                           const unsigned char *m, unsigned long long mlen)
 {
   // dom2(x, y) with x = 0 (not prehashed) and y = "crypto_tweak_ed25519"
@@ -26,7 +26,7 @@ void _crypto_tweak_nonce (unsigned char *nonce, const unsigned char *sk,
 
   crypto_hash_sha512_init(&hs);
   crypto_hash_sha512_update(&hs, TWEAK_PREFIX, sizeof TWEAK_PREFIX);
-  crypto_hash_sha512_update(&hs, sk, 32);
+  crypto_hash_sha512_update(&hs, n, 32);
   crypto_hash_sha512_update(&hs, m, mlen);
   crypto_hash_sha512_final(&hs, nonce);
 }
@@ -55,25 +55,36 @@ void crypto_tweak_ed25519(unsigned char *n, unsigned char *q,
 {
   unsigned char n64[64];
 
-  crypto_hash(n64, ns, nslen);
-  n64[31] &= 127; // clear highest bit
-
-  crypto_scalarmult_ed25519_base_noclamp(q, n64);
-
-  // hash tweak until we get a valid tweaked point
-  while (crypto_core_ed25519_is_valid_point(q) != 1) {
-    crypto_hash(n64, n64, 32);
-    n64[31] &= 127; // clear highest bit
-
-    crypto_scalarmult_ed25519_base_noclamp(q, n64);
-  }
+  _crypto_tweak_ed25519(n64, q, ns, nslen);
 
   SN_TWEAK_COPY_32(n, n64)
 }
 
+void crypto_tweak_ed25519_keypair(unsigned char *pk, unsigned char *scalar_out,
+                                  unsigned char *scalar, const unsigned char *ns,
+                                  unsigned long long nslen)
+{
+  unsigned char n64[64];
+
+  crypto_hash(n64, ns, nslen);
+  n64[31] &= 127; // clear highest bit
+
+  crypto_tweak_ed25519_scalar_add(scalar_out, scalar, n64);
+  crypto_scalarmult_ed25519_base_noclamp(pk, scalar_out);
+
+  // hash tweak until we get a valid tweaked point
+  while (crypto_core_ed25519_is_valid_point(pk) != 1) {
+    crypto_hash(n64, n64, 32);
+    n64[31] &= 127; // clear highest bit
+
+    crypto_tweak_ed25519_scalar_add(scalar_out, scalar, n64);
+    crypto_scalarmult_ed25519_base_noclamp(pk, scalar_out);
+  }
+}
+
 int crypto_tweak_ed25519_sign_detached(unsigned char *sig, unsigned long long *siglen_p,
                                        const unsigned char *m, unsigned long long mlen,
-                                       const unsigned char *sk)
+                                       const unsigned char *n)
 {
   crypto_hash_sha512_state hs;
 
@@ -83,12 +94,12 @@ int crypto_tweak_ed25519_sign_detached(unsigned char *sig, unsigned long long *s
   unsigned char            hram[64];
 
 
-  // derive pk from sk
-  if (crypto_scalarmult_ed25519_base_noclamp(pk, sk) != 0) {
+  // derive pk from scalar
+  if (crypto_scalarmult_ed25519_base_noclamp(pk, n) != 0) {
     return -1;
   }
 
-  _crypto_tweak_nonce(nonce, sk, m, mlen);
+  _crypto_tweak_nonce(nonce, n, m, mlen);
   crypto_core_ed25519_scalar_reduce(nonce, nonce);
 
   // R = G ^ nonce : curve point from nonce
@@ -106,8 +117,8 @@ int crypto_tweak_ed25519_sign_detached(unsigned char *sig, unsigned long long *s
 
   crypto_core_ed25519_scalar_reduce(hram, hram);
 
-  // sig = nonce + sk * h(ram) 
-  crypto_core_ed25519_scalar_mul(sig, hram, sk);
+  // sig = nonce + n * h(ram)
+  crypto_core_ed25519_scalar_mul(sig, hram, n);
   crypto_core_ed25519_scalar_add(sig + 32, nonce, sig);
 
   SN_TWEAK_COPY_32(sig, R)
@@ -139,18 +150,13 @@ void crypto_tweak_ed25519_secretkey(unsigned char *scalar,
                                     const unsigned char *ns,
                                     unsigned long long nslen)
 {
-  unsigned char _sk[64];
   unsigned char n[64];
   unsigned char q[32];
 
-  // get sk scalar from seed, cf. crypto_sign_keypair_seed
-  crypto_hash(_sk, sk, 32);
-  _sk[0] &= 248;
-  _sk[31] &= 127;
-  _sk[31] |= 64;
+  crypto_tweak_ed25519_sk_to_scalar(scalar, sk);
 
   _crypto_tweak_ed25519(n, q, ns, nslen);
-  crypto_core_ed25519_scalar_add(scalar, n, _sk);
+  crypto_core_ed25519_scalar_add(scalar, n, scalar);
 }
 
 // tweak a public key
@@ -166,12 +172,12 @@ int crypto_tweak_ed25519_publickey(unsigned char *tpk,
   return crypto_core_ed25519_add(tpk, q, pk);
 }
 
-// add tweak to private key
-void crypto_tweak_ed25519_secretkey_add(unsigned char *scalar,
-                                        const unsigned char *sk,
-                                        const unsigned char *n)
+// add tweak to scalar
+void crypto_tweak_ed25519_scalar_add(unsigned char *scalar_out,
+                                     const unsigned char *scalar,
+                                     const unsigned char *n)
 {
-  crypto_core_ed25519_scalar_add(scalar, n, sk);
+  crypto_core_ed25519_scalar_add(scalar_out, scalar, n);
 }
 
 // add tweak point to public key
