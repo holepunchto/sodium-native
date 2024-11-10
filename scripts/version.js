ba866c756f957c25b475ce23fb9cf5e1d91ccbac
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-10 12:59:20 +0100

    Use `cmake-fetch` for dependencies

--- scripts/version.js
+++ scripts/version.js
@@ -1,18 +1,16 @@
 const path = require('path')
 const fs = require('fs')
 
-const root = path.join(__dirname, '..')
-
-const libsodium = path.join(root, 'vendor/libsodium/src/libsodium')
+const [sodium] = process.argv.slice(2)
 
 const version = '1.0.18'
 
-const file = fs.readFileSync(path.join(libsodium, 'include/sodium/version.h.in'), 'utf-8')
+const file = fs.readFileSync(path.join(sodium, 'src/libsodium/include/sodium/version.h.in'), 'utf-8')
   .replace('@VERSION@', version)
-  .replace('@SODIUM_LIBRARY_VERSION_MAJOR@', 10)
-  .replace('@SODIUM_LIBRARY_VERSION_MINOR@', 3)
+  .replace('@SODIUM_LIBRARY_VERSION_MAJOR@', 26)
+  .replace('@SODIUM_LIBRARY_VERSION_MINOR@', 2)
   .replace('@SODIUM_LIBRARY_MINIMAL_DEF@', '')
 
-fs.writeFileSync(path.join(libsodium, 'include/sodium/version.h'), file)
+fs.writeFileSync(path.join(sodium, 'src/libsodium/include/sodium/version.h'), file)
 
 process.stdout.write(version)
