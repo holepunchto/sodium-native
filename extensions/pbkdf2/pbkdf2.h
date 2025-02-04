60827168aa9f62507781aa59e13b909d8a77f29c
Luz Paz <luzpaz@pm.me>
2025-02-04 07:37:57 -0500

    Fix typos

--- extensions/pbkdf2/pbkdf2.h
+++ extensions/pbkdf2/pbkdf2.h
@@ -26,7 +26,7 @@
  */
 
 /*
- * Adapated from libsodium/crypto_pwhash/scryptsalsa208sha256/pbkdf-sha256.c
+ * Adapted from libsodium/crypto_pwhash/scryptsalsa208sha256/pbkdf-sha256.c
  */
 
 #include <sodium.h>
